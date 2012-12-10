34a3f3d0a2507e2c7be12a0d3e49a5fe64697c49
Travis Swicegood <development@domain51.com>
2012-12-09 21:32:41 -0600

    Add test cases for get_start_and_end_dates()

--- example/example_usage/tests/__init__.py
+++ example/example_usage/tests/__init__.py
@@ -1,4 +1,5 @@
 from .managers import *
 from .models import *
 from .query import *
+from .scraper import *
 from .views import *
---
+++ example/example_usage/tests/scraper.py
@@ -0,0 +1,48 @@
+import datetime
+import random
+
+from django.test import TestCase
+from mock import patch, MagicMock
+
+from atx_restaurant_ratings import scraper
+
+
+def random_date():
+    now = datetime.date.today()
+    r = random.randint(-1000, 1000)
+    return (now - datetime.timedelta(days=r)).strftime('%m/%d/%Y')
+
+
+def generate_random_start_end_doc(start_date=None, end_date=None):
+    if start_date is None:
+        start_date = random_date()
+    if end_date is None:
+        end_date = random_date()
+
+    start = MagicMock(text=start_date)
+    end = MagicMock(text=end_date)
+    find = MagicMock(return_value=[start, end])
+    return start_date, end_date, MagicMock(find=find)
+
+
+class TestOf_get_start_and_end_dates(TestCase):
+    def test_first_value_is_the_start_date(self):
+        start_date, _, doc = generate_random_start_end_doc()
+        with patch.object(scraper, 'pq') as mock:
+            mock.return_value = doc
+            result = scraper.get_start_and_end_dates()
+            self.assertEqual(result[0], start_date)
+
+    def test_second_value_is_the_end_date(self):
+        _, end_date, doc = generate_random_start_end_doc()
+        with patch.object(scraper, 'pq') as mock:
+            mock.return_value = doc
+            result = scraper.get_start_and_end_dates()
+            self.assertEqual(result[1], end_date)
+
+    def test_strips_any_extranous_whitespace_off_of_values(self):
+        a, b, doc = generate_random_start_end_doc(' Alice', 'Bob ')
+        with patch.object(scraper, 'pq') as mock:
+            mock.return_value = doc
+            actual = scraper.get_start_and_end_dates()
+            self.assertEqual(['Alice', 'Bob'], actual)
